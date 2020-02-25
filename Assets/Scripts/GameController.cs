32643d3a9251cc650c66f260ed1f69bd3e80a337
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-02-25 13:31:59 -0800

    Adding Finite-State Machine

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStates
+{
+    INGAME,
+    MAINMENU,
+    GAMEOVER,
+    SETTINGS
+}
+
+public class GameController : MonoBehaviour
+{
+    //Game starts in MAINMENU
+    private GameStates currentState = GameStates.MAINMENU;
+
+    //UI Groups
+    public GameObject MainMenu;
+    public GameObject Ingame;
+    public GameObject GameOver;
+    public GameObject Settings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case GameStates.MAINMENU:
+            {
+                //Show Menu UI
+                //Hide everything else
+            }
+            break;
+
+            case GameStates.INGAME:
+            {
+                //Show Gameplay UI
+                //Hide everything else
+            }
+            break;
+
+            case GameStates.GAMEOVER:
+            {
+                //Show GameOver UI
+                //Hide everything else
+            }
+            break;
+
+            case GameStates.SETTINGS:
+            {
+                //Show settings UI
+                //Hide everything else
+            }
+            break;
+        }
+    }
+}
--- Assets/Scripts/Offset.cs
+++ Assets/Scripts/Offset.cs
@@ -8,15 +8,18 @@ public class Offset : MonoBehaviour
 
     public float speed = 2;
     public float offset;
+
     // Start is called before the first frame update
     void Start()
     {
+
         currentMaterial = GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Modifying texture's offset to give an effect of movement
         offset += 0.001f;
         currentMaterial.SetTextureOffset("_MainTex", new Vector3(0, - offset * (speed + 1), 0));
     }
