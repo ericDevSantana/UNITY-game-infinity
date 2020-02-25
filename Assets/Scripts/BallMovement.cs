c57ddc8169b993eea1f2b4cadd52d11527eeb8e5
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-02-25 00:06:10 -0800

    First Commit

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed;
+    public float x = 0;
+    public float factor;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (x > 1000) { x = 0; }
+        x += Time.deltaTime * factor;
+        transform.rotation = Quaternion.Euler(x,0,0);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            gameObject.GetComponent<Rigidbody>().AddForce(-speed, 0, 0, ForceMode.Impulse);
+            //gameObject.transform.position = new Vector3();
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            gameObject.GetComponent<Rigidbody>().AddForce(+speed, 0, 0, ForceMode.Impulse);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Offset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Offset : MonoBehaviour
+{
+    private Material currentMaterial;
+
+    public float speed = 2;
+    public float offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMaterial = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset += 0.001f;
+        currentMaterial.SetTextureOffset("_MainTex", new Vector3(0, - offset * (speed + 1), 0));
+    }
+}
